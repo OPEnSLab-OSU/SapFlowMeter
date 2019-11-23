2a6ff20bafa6350c95dfaf48a6f564ccbe435415
Marshal Horn <kamocat@gmail.com>
2019-11-23 00:52:29 -0800

    Attempting to parse dates. Not working very well

--- analysis/plot_sap.py
+++ analysis/plot_sap.py
@@ -7,7 +7,12 @@ import numpy as np
 def load_data( filename, index=0 ):
 	print("Loading data")
 	# Ignore the date column for now
-	data = pd.read_csv(filename, memory_map=True, usecols = (1,2), skiprows=index )
+	data = pd.read_csv(filename, memory_map=True,
+	nrows=2000 )
+#	print("Interpreting dates")
+#	dates = pd.to_datetime(data.iloc[:,0], format="%Y-%m-%d %H:%M:%S")
+#	return (data.iloc[:,1:],dates)
+	data.set_index("Date")
 	return data
 
 
@@ -19,7 +24,8 @@ def calculate_flow( upper, lower ):
 	upper_slope = upper[l:].mean() - upper[:l].mean()
 	lower_slope = lower[l:].mean() - upper[:l].mean()
 	#print("Upper slope: ", upper_slope, "Lower slope: ", lower_slope)
-	return k * np.log(upper_slope/lower_slope)
+	flow = k * np.log(upper_slope/lower_slope)
+	return (flow, upper_slope, lower_slope)
 
 
 # The input is a pandas series
@@ -34,20 +40,23 @@ def find_peaks( data ):
 	period = 990 # 5 minute period
 	rise_time = 150 # 40 second Rise time
 	size = data.shape[0]
-	flowrate = np.empty(int(size/period)+1)
+	flowrate = np.empty((int(size/period+1), 3))
 	j = 0
 	for i in range(0,size,period):
 		end = i+rise_time
-		flowrate[j]=calculate_flow(data.iloc[i:end, 0],data.iloc[i:end, 1])
+		flowrate[j,:]=calculate_flow(data.iloc[i:end, 0],data.iloc[i:end, 1])
 		j = j + 1
 	return flowrate
 	
+def main():
+	data = load_data("sapflow.csv", index=22) # The first 22 datapoints are junk
 
-data = load_data("sapflow.csv", index=22) # The first 22 datapoints are junk
+	print("Calculating sap flow")
+	flowrate = find_peaks(data)
 
-print("Calculating sap flow")
-flowrate = find_peaks(data)
+	print("Plotting data")
+	plt.plot(flowrate[:,1:])
+	plt.show()
 
-print("Plotting data")
-plt.plot(flowrate)
-plt.show()
+
+data = load_data("sapflow.csv")
