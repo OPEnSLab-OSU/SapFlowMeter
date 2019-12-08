714f1c535d6871d1ba4ab4d776dc2a15b93413c0
Marshal Horn <kamocat@gmail.com>
2019-12-07 19:42:38 -0800

    Huge improvement in linear regression over naive slope

--- analysis/linear_fit.py
+++ analysis/linear_fit.py
@@ -3,27 +3,30 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 import slope
+import math
 
+condensed = pd.read_csv('../data/sapflow(06).csv', index_col=0,
+  parse_dates = [0], infer_datetime_format = True, usecols=[0, 5] )
+condensed.plot()
+dates = condensed.index
+length = condensed.shape[0]
 #fname = 'sapflow(06).csv'
 fname = '../data/temperature_log.csv'
 
 data = pd.read_csv(fname , index_col=0, #nrows = 1000,
   parse_dates =[0], infer_datetime_format = True)
 
-p = dt.timedelta(minutes=5)
-l = dt.timedelta(minutes=1)
-h = data.index[-1] - l
-# Print segments of data with the linear regression line
-while 1:
-  a = data.loc[h:h+l,:]
-  a.plot()
-  size = a.shape
-  x = np.arange(size[0])
-  c = slope.linreg(a.iloc[:,0])
-  y = x*c[1]+c[0]
-  plt.plot(a.index, y)
-  c = slope.linreg(a.iloc[:,1])
-  y = x*c[1]+c[0]
-  plt.plot(a.index, y)
-  plt.show()
-  h -= p
+p = dt.timedelta(minutes=1)
+sapflow = np.empty(length)
+for i in range(length):
+  t = dates[i]
+  a = data.loc[t-p:t,:]
+  c1 = slope.linreg(a.iloc[:,0])
+  c2 = slope.linreg(a.iloc[:,1])
+  try:
+    sapflow[i] = math.log(c1[1]/c2[1])
+  except:
+    sapflow[i] = -1
+
+plt.plot(dates,sapflow)
+plt.show()
