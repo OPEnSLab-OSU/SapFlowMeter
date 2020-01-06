d463eac27bb3d601f51c09a3379f9f868fc7729f
Marshal Horn <kamocat@gmail.com>
2020-01-06 12:34:20 -0800

    Use improved calculation to determine sapflow, and extract weight for comparison

--- analysis/improved_calc.py
+++ analysis/improved_calc.py
@@ -17,23 +17,29 @@ def improved_sapflow( dates, data, seconds=50):
     c2 = slope.linreg(a.iloc[:,1])
     # Sapflow is the log of the ratio of the slopes
     sapflow[i] = math.log(c1[1]/c2[1])
-  plt.plot(dates,sapflow)
+  #plt.plot(dates,sapflow)
   return sapflow
 
 # Load the naive sapflow data
-condensed = pd.read_csv('../data/sapflow(06).csv', index_col=0,
-  parse_dates = [0], infer_datetime_format = True, usecols=[0, 5] )
-condensed.plot()
+condensed = pd.read_csv('../data/sapflow(02).csv', index_col=0,
+  parse_dates = [0], infer_datetime_format = True, usecols=[0, 6] )
 dates = condensed.index
 length = condensed.shape[0]
-#fname = 'sapflow(06).csv'
 fname = '../data/temperature_log.csv'
+print("Read condensed data")
+
 
 # Load the whole measurement log
 data = pd.read_csv(fname , index_col=0, #nrows = 1000,
   parse_dates =[0], infer_datetime_format = True)
 
-improved_sapflow(dates, data)
+print("Loaded the entire temperature log")
+
+flow = improved_sapflow(dates, data)
+print("Calculated sapflow")
+condensed['sapflow']=flow
+condensed.to_csv("good.csv")
+print("Saved data")
 
 # Fixme: Make prettier plots
-plt.show()
+#plt.show()
