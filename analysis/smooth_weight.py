14099ed773fa34d7e71cdc990f55d3e5a03d4ef3
Marshal Horn <kamocat@gmail.com>
2020-01-11 09:58:58 -0800

    fixed discontinuities which occur during watering

--- analysis/smooth_weight.py
+++ analysis/smooth_weight.py
@@ -4,8 +4,18 @@ import numpy as np
 import scipy.signal as sig
 
 # Load the naive sapflow data
-condensed = pd.read_csv('../data/sapflow(04).csv', index_col=0,
+condensed = pd.read_csv('../data/sapflow(02).csv', index_col=0,
   parse_dates = [0], infer_datetime_format = True, usecols=[0, 6] )
+
+print("Resolving discontinuity")
+w = condensed[' weight']
+a = np.ediff1d(w)
+b = np.nonzero( a > 0.5 )
+print(F'Fixing at indexes {b}')
+for i in b[0]:
+  w.iloc[i+1:] -= a[i]
+
+condensed[' weight'] = w
 
 print("Filtering out quantization noise")
 f = sig.cheby2(8, 100, .5, fs=12, output='sos')
@@ -22,12 +32,12 @@ fig, ax1 = plt.subplots()
 ax1.set_xlabel('Date')
 ax1.set_ylabel('Weight (kg)')
 ax1.plot(condensed)
-#ax1.legend('weight', 'smoothed')
 print("Second axis")
+yellow = (1,.8,0)
 ax2 = ax1.twinx()
+ax2.set_facecolor(yellow)
 condensed['water used']=b
 ax2.set_ylabel('water used')
-ax2.plot(condensed.index, b, color=(1,.8,0))
-#ax2.legend('water usage')
+ax2.plot(condensed.index, b, color=yellow)
 fig.tight_layout()
 plt.show()
