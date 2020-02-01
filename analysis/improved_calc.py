Fixed issues with sapflow calculation
- Excluded data while the heater probe is on. The heater still seems to present electrical conduction issues for the RTDs.
- Included upper and lower slope and baseline in processed data. This is useful for determining what the issues are that are causing wonky sapflow results.

analysis/improved_calc.py
@@ -6,23 +6,28 @@ import slope
 import math
 
 # Test sapflow calculation with linear regression to find slope
-def improved_sapflow( dates, data, seconds=50):
+def improved_sapflow( dates, data, seconds=40):
   p = dt.timedelta(seconds=seconds)
-  sapflow = np.empty(length)
+  sapflow = np.empty((length, 5))
   for i in range(length):
     t = dates[i] #Get the timestamp for this index
     a = data.loc[t-p:t,:] # Get the data corresponding to that date range
     # Calculate the slope using linear regression
     c1 = slope.linreg(a.iloc[:,0])
     c2 = slope.linreg(a.iloc[:,1])
+    sapflow[i,1:3] = c1
+    sapflow[i,3:5] = c2
     # Sapflow is the log of the ratio of the slopes
-    sapflow[i] = math.log(c1[1]/c2[1])
+    try:
+      sapflow[i,0] = math.log(c1[1])-math.log(c2[1])
+    except:
+      sapflow[i,0] = 0
   #plt.plot(dates,sapflow)
   return sapflow
 
 # Load the naive sapflow data
-condensed = pd.read_csv('../data/sapflow(02).csv', index_col=0,
-  parse_dates = [0], infer_datetime_format = True, usecols=[0, 6] )
+condensed = pd.read_csv('../data/sapflow(05).csv', index_col=0,
+  parse_dates = [0], infer_datetime_format = True, usecols=[0,6] )
 dates = condensed.index
 length = condensed.shape[0]
 fname = '../data/temperature_log.csv'
@@ -37,9 +42,14 @@ print("Loaded the entire temperature log")
 
 flow = improved_sapflow(dates, data)
 print("Calculated sapflow")
-condensed['sapflow']=flow
+condensed['sapflow']=flow[:,0]
+condensed['upper baseline']=flow[:,1]
+condensed['upper slope']=flow[:,2]
+condensed['lower baseline']=flow[:,3]
+condensed['lower slope']=flow[:,4]
 condensed.to_csv("good.csv")
 print("Saved data")
 
 # Fixme: Make prettier plots
-#plt.show()
+condensed.plot()
+plt.show()

analysis/smooth_weight.py
@@ -4,8 +4,8 @@ import numpy as np
 import scipy.signal as sig
 
 # Load the naive sapflow data
-condensed = pd.read_csv('../data/sapflow(04).csv', index_col=0,
-  parse_dates = [0], infer_datetime_format = True, usecols=[0, 6] )
+condensed = pd.read_csv('jan6.csv', index_col=0,
+  parse_dates = [0], infer_datetime_format = True )
 
 print("Resolving discontinuity")
 w = condensed[' weight']
@@ -24,6 +24,8 @@ condensed['smoothed weight'] = a
 
 print("Taking Derivative")
 b = -1 * 3600. / 7. * sig.savgol_filter(a, 31, 1, 1, axis=0)
+condensed['gravimetric'] = b
+condensed.to_csv('pretty.csv')
 
 print("Printing graph")
 from pandas.plotting import register_matplotlib_converters