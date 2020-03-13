2c9584ac8eeb463747dbd00f2adc1b9f9fd24b30
Marshal Horn <kamocat@gmail.com>
2020-03-13 14:42:48 -0700

    All three temperatures look correct now.
    I changed the output to show drift in millidegrees, so we can see something comparable to the measurements in a tree.

--- mcp3424_code/mcp3424_code.ino
+++ mcp3424_code/mcp3424_code.ino
@@ -1,14 +1,21 @@
 #include "Sapflow_ADC.h"
 
-void setup() {
-  Serial.begin(115200);
-}
-
+struct temperature baseline;
 uint8_t addr = 0x6E;
 struct temperature c;
+
 struct pt * pt;
+
+void setup() {
+  Serial.begin(115200);
+  get_temp(pt, addr, &baseline);
+}
+
 void loop() {
   get_temp(pt, addr, &c);
+  c.upper = (c.upper - baseline.upper) * 1000;
+  c.lower = (c.lower - baseline.lower) * 1000;
+  c.heater = (c.heater - baseline.heater) * 1000;
   Serial.print("Upper:");
   Serial.print(c.upper);
   Serial.print(",Lower:");
