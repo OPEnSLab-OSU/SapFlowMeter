0dff23d07ecd6f81e9f419326755fab9e6de1990
Marshal Horn <kamocat@gmail.com>
2020-03-12 17:18:53 -0700

    Included FeatherFault in LoRa base station

--- Feather9x_RX/Feather9x_RX.ino
+++ Feather9x_RX/Feather9x_RX.ino
@@ -6,6 +6,7 @@
 #include <RH_RF95.h>
 #include <Ethernet.h>
 #include <ArduinoJson.h>
+#include <FeatherFault.h>
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
@@ -108,36 +109,42 @@ bool printWebData = true;  // set to false for better speed measurement
 
 void setup()
 {
+  FeatherFault::StartWDT(FeatherFault::WDTTimeout::WDT_4S);
+  MARK;
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
+  MARK;
   Serial.begin(115200);
-  while (!Serial) {
-    delay(1);
-  }
   delay(100);
 
   Serial.println("Feather LoRa RX Test!");
+  MARK;
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
+  MARK;
 
   while (!manager.init()) {
     Serial.println("LoRa radio init failed");
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    MARK;
     while (1);
   }
+  MARK;
   Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
+    MARK;
     while (1);
   }
+  MARK;
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
@@ -146,6 +153,7 @@ void setup()
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+  MARK;
 }
 
 uint8_t data[] = "And hello back to you";
@@ -154,13 +162,16 @@ uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 void loop()
 {
+  MARK;
   if (manager.available())
   {
+    MARK;
     // Wait for a message addressed to us from the client
     uint8_t len = sizeof(buf);
     uint8_t from;
     if (manager.recvfromAck(buf, &len, &from))
     {
+      MARK;
       digitalWrite(LED, HIGH);
       Serial.print("got request from : 0x");
       Serial.print(from, HEX);
@@ -168,75 +179,111 @@ void loop()
       Serial.println((char*)buf);
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
+      MARK;
 
       // Send a reply back to the originator client
       if (!manager.sendtoWait(data, sizeof(data), from))
-        Serial.println("sendtoWait failed");
+        Serial.println("sendtoWait failed"); MARK;
       digitalWrite(LED, LOW);
+      MARK;
     }
+  MARK;
      // You can use Ethernet.init(pin) to configure the CS pin
   Ethernet.init(10);  // Most Arduino shields [including Feather M0]
   pinMode(RFM95_RST, INPUT_PULLUP);
+  MARK;
 
   Serial.println("Initialize Ethernet with DHCP:");
   if (Ethernet.begin(mac) == 0) {
+    MARK;
     Serial.println("Failed to configure Ethernet using DHCP");
     // Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      MARK;
       Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
       while (true) {
         delay(1); // do nothing, no point running without Ethernet hardware
       }
     }
+    MARK;
     if (Ethernet.linkStatus() == LinkOFF) {
       Serial.println("Ethernet cable is not connected.");
     }
+    MARK;
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip, myDns);
+  MARK;
   } else {
+    MARK;
     Serial.print("  DHCP assigned IP ");
     Serial.println(Ethernet.localIP());
   }
+  MARK;
     delay(1000);
+  MARK;
   Serial.print("connecting to ");
   Serial.print(server);
   Serial.println("...");
+  MARK;
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
+    MARK;
     Serial.print("connected to ");
     Serial.println(client.remoteIP());
+    MARK;
     // Make a HTTP request:
     char input[] = "{\"flow\":\"11.3\",\"weight\":\"7.8\",\"temp\":\"27.5\",\"time\":\"15:30\",\"id\":\"15\"}";
+    MARK;
     const int capacity = JSON_OBJECT_SIZE(5);
+    MARK;
     StaticJsonDocument<capacity> doc;
+    MARK;
     DeserializationError err = deserializeJson(doc, (char*)buf);
+    MARK;
     if (err) {
+      MARK;
       Serial.print(F("deserializeJson() failed with code "));
       Serial.println(err.c_str());
     }
+    MARK;
 
     const char * flow = doc["flow"];
+    MARK;
     const char * weight = doc["weight"];
+    MARK;
     const char * temp = doc["temp"];
+    MARK;
     const char * time1 = doc["time"];
+    MARK;
     const char * id = doc["id"];
+    MARK;
   
   char url[100];
+    MARK;
    sprintf(url, "GET /~veselyv/new.php?flow=%s&weight=%s&temp=%s&time=%s&id=%s HTTP/1.1",flow,weight,temp,time1,id);
+    MARK;
    client.println(url);
+   MARK;
    Serial.print("Sent url: ");
    Serial.println(url);
+   MARK;
    client.println("Host: web.engr.oregonstate.edu");
    client.println("Connection: close");
    client.println();
+   MARK;
    
   } else {
+    MARK;
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
+  MARK;
   delay(1000);
+  MARK;
   client.stop();
+  MARK;
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
   }
+  MARK;
 }
