f416ba0f899400a4c9168af102b427f6f0e8ed56
Marshal Horn <kamocat@gmail.com>
2020-03-13 12:58:47 -0700

    Simplified MCP example

--- mcp3424_code/Sapflow_ADC.h
+++ mcp3424_code/Sapflow_ADC.h
@@ -23,8 +23,20 @@ struct temperature{
     double upper;   //temperature in deg C at upper probe
     double lower;   //temperature in deg C at lower probe
     double heater;  //temperature in deg C at heater probe
-}
+};
+
+enum register_mask{
+  RDY=1<<7,  // result ready
+  CHAN=3<<5, // Channel 0-3
+  CON=1<<4,  // Continuous/One-Shot
+  DEPTH=3<<2,  // Bit depth 12-18
+  PGA=3<<0,   // Gain of 1-8
+};
 
 int get_temp(struct pt *pt, uint8_t i2c_addr, struct temperature *dest);
+
+double rtd_calc(int32_t raw);
+
+int32_t measure(uint8_t addr, uint8_t channel);
 
 #endif
