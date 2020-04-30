b3c1314b252dc3b28d52c2aec54c984099196802
Marshal Horn <kamocat@gmail.com>
2020-04-30 13:27:48 -0700

    Relaxed watchdog requirements
    Watchdog timer is increased to 4 seconds.
    Schedule now sleeps immidiately, so repeated watchdog faults shouldn't kill the battery.
    Fixed sleep.cpp to log with plog rather than cout.

--- sapflow_protothread/sleep.cpp
+++ sapflow_protothread/sleep.cpp
@@ -12,7 +12,7 @@ void feather_sleep( void ){MARK;
   // Wait for the alarm to clear
   while(!digitalRead(ALARM_PIN)){
     pinMode(ALARM_PIN, INPUT_PULLUP);
-    Serial.print("Waiting on alarm pin...");
+    PLOG_DEBUG << "Waiting on alarm pin...";
     delay(10);
   }MARK;
   hardware_deinit();
@@ -29,18 +29,18 @@ void feather_sleep( void ){MARK;
 // Sleep until the time is a round multiple of the minute inteval.
 void sleep_cycle( int interval, int offset ){
   MARK;
-  cout<<"Sleeping until nearest multiple of "<<interval
+  PLOG_INFO<<"Sleeping until nearest multiple of "<<interval
   <<" minutes with "<<offset<<" seconds offset"<<endl;
   MARK;
   DateTime t = rtc_ds.now();MARK;
   interval *= 60;
   // Roll back to start of this interval using integer floor
   t = DateTime((t.unixtime()/interval)*interval);
-  cout<<"Rolled back to "<<t.text()<<endl;
+  PLOG_VERBOSE<<"Rolled back to "<<t.text()<<endl;
   // Add interval and offset
   t = t + TimeSpan( interval + offset);MARK;
   rtc_ds.setAlarm(t);MARK;
-  cout<<"Alarm set to "<<rtc_ds.getAlarm(1).text()<<endl;
+  PLOG_DEBUG<<"Alarm set to "<<rtc_ds.getAlarm(1).text()<<endl;
   MARK;
   feather_sleep();MARK;
 }
